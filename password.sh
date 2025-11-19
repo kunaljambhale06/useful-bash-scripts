79ae04ec21a2230c1a68cf0980a501d4abef5690
Kunal Jambhale <jambhalekunal06@gmail.com>
2025-11-19 20:29:54 +0530

    Add password generation script

---
+++ password.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Function to generate a random password
+generate_password() {
+    tr -dc 'A-Za-z0-9!@#$%^&*()_+{}[]' < /dev/urandom | fold -w 12 | head -n 1
+}
+
+# Call the function and store the generated password
+password=$(generate_password)
+echo "Generated password: $password"
