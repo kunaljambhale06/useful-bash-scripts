942b8013f225327b23ac0c37b5f9ffd43e6f4a19
Kunal Jambhale <jambhalekunal06@gmail.com>
2026-01-04 12:32:55 +0530

    Add input script to greet user by name

---
+++ input.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Ask the user for their name
+echo "What's your name?"
+read name
+# Greet the user
+echo "Hello, $name! Nice to meet you."
+~                                                                               
+~                                                                               
+                          
